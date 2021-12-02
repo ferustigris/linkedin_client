2f69de95799be0fdbd0b40fa7622b348c9a7245c
Alex Evseenko <alex.evseenko@gmail.com>
2021-12-02 19:46:37 +0100

    Add /gists query for github.

--- src/main/java/ferus/tigris/queries/GithubQuery.java
+++ src/main/java/ferus/tigris/queries/GithubQuery.java
@@ -10,6 +10,7 @@ public class GithubQuery extends BaseQuery {
         super(API_DATA, "/organizations");
         // init meta-info about other endpoints
         this.endpoints.put("/search/code", parseParams("q=addClass+in:file+language:js+repo:jquery/jquery"));
+        this.endpoints.put("/gists", parseParams("q=page=10&per_page=1"));
     }
 
     public GithubQuery(String path, String... properties) {
