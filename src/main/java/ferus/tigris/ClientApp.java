98b4d75bad42ed951e9aaab6b461d4616195afa5
Andrey_Kuznetsov <ferus.tigris@gmail.com>
2022-01-04 18:53:37 +0100

    CLI arguments parsing has been refactored

--- src/main/java/ferus/tigris/ClientApp.java
+++ src/main/java/ferus/tigris/ClientApp.java
@@ -7,31 +7,33 @@ public class ClientApp {
 
     public static void main(String[] args) {
         Client client = new Client();
+        BaseQuery query = null;
+
+        for(String arg: args) {
+            if ("--help".equals(arg)) {
+                System.out.println("Usage: java ferus.tigris.Client [--api=api-name] [--query=query-params...]");
+                System.exit(1);
+            } else if (arg.startsWith("--api=")) {
+                // find out which API to instantiate
+                String apiName = arg.substring(6);
+                query = client.getQuery(apiName);
+                if (query == null) {
+                    System.out.println("Not defined API: " + apiName);
+                    System.exit(1);
+                }
+                System.out.println("API to query: " + query.getRequestUrl());
+            }
+        }
+
+        // Check if application configured correctly
         if (args.length == 0) {
             // list of available APIs
             System.out.println(client.getReposList());
             System.exit(0);
-        } else if ("--help".equals(args[0])) {
-            System.out.println("Usage: java ferus.tigris.Client [--api=api-name] [--query=query-params...]");
-            System.exit(1);
-        }
-
-        BaseQuery query = null;
-        // find out which API to instantiate
-        if (args[0].startsWith("--api=")) {
-            String apiName = args[0].substring(6);
-            query = client.getQuery(apiName);
-            if (query == null) {
-                System.out.println("Not defined API: " + apiName);
-                System.exit(1);
-            }
-            System.out.println("API to query: " + query.getRequestUrl());
-        } else {
+        } else if (query == null) {
             System.out.println("Set up API name in --api parameter.");
             System.exit(1);
-        }
-
-        if (args.length == 1) {
+        } else if (args.length == 1) {
             // list of queries available for the chosen API
             System.out.println("Available endpoints:\n\t" +
                     String.join(", \t", query.endpoints()));
